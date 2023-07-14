1430d438c2c6ad8f99f41ade06d808074f9ff81b
JackSlice-2 <jackslice9@gmail.com>
2023-07-14 06:31:06 -0300

    first commit

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs text-black">
+                    &copy; 2023 Bazar do Cello, All rights reserved || Developed by AfroTech
+                </p>
+            </div>
+
+        </footer>
+    );
+}
+
+export default Footer;
