39d2f34c19eff12289981935d45d560dd963f2e8
JackSlice-2 <jackslice9@gmail.com>
2023-07-16 07:04:01 -0300

    Billboard if statement in case undefined billboardID

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -4,26 +4,27 @@ import Billboard from "@/components/billboard";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
-export const revalidate = 0
+export const revalidate = 0;
 
 const HomePage = async () => {
-    const billboardId = process.env.REACT_APP_BILLBOARD_ID;
-    const products = await getProducts({ isFeatured: true });
-    const billboard = await getBillboard(billboardId);
+  const billboardId = process.env.REACT_APP_BILLBOARD_ID;
+  const products = await getProducts({ isFeatured: true });
 
-    return (
-        <Container>
-            <div className="space-y-10 pb-10">
-                <Billboard data={billboard} />
-                <div 
-                className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    <ProductList 
-                    title="Featured Products" 
-                    items={products} />
-            </div>
+  let billboard = null;
+  if (billboardId) {
+    billboard = await getBillboard(billboardId);
+  }
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        {billboard && <Billboard data={billboard} />}
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
         </div>
-        </Container>
-    )
-}
+      </div>
+    </Container>
+  );
+};
 
 export default HomePage;
