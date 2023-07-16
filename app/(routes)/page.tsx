6e816e5ed84d8f62047f0a5ec0b031df03b1246e
JackSlice-2 <jackslice9@gmail.com>
2023-07-16 06:51:06 -0300

    BillboardID changed to env variables

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,15 +7,19 @@ import Container from "@/components/ui/container";
 export const revalidate = 0
 
 const HomePage = async () => {
+    const billboardId = process.env.REACT_APP_BILLBOARD_ID;
     const products = await getProducts({ isFeatured: true });
-    const billboard = await getBillboard('801dbbb4-e47b-42f7-9612-12a38f0a1b5a')
+    const billboard = await getBillboard(billboardId);
 
     return (
         <Container>
             <div className="space-y-10 pb-10">
                 <Billboard data={billboard} />
-                <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                    <ProductList title="Featured Products" items={products} />
+                <div 
+                className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                    <ProductList 
+                    title="Featured Products" 
+                    items={products} />
             </div>
         </div>
         </Container>
