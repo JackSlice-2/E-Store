8f215ca4e91b3671db0ebb9986d5338c5960c333
JackSlice-2 <jackslice9@gmail.com>
2023-07-22 13:22:13 -0300

    details, bugs

--- components/info.tsx
+++ components/info.tsx
@@ -27,23 +27,23 @@ const Info: React.FC<InfoProps> = ({
 
   return ( 
     <div>
-      <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
+      <h1 className="text-3xl font-bold text-gray-900 dark:text-white">{data.name}</h1>
       <div className="mt-3 flex items-end justify-between">
-        <p className="text-2xl text-gray-900">
+        <p className="text-2xl text-gray-900 dark:text-white">
           <Currency value={data?.price} />
         </p>
       </div>
       <hr className="my-4" />
       <div className="flex flex-col gap-y-6">
         <div className="flex items-center gap-x-4">
-          <h3 className="font-semibold text-black">Size:</h3>
+          <h3 className="font-semibold text-black dark:text-white">Size:</h3>
           <div>
             {data?.size?.value}
           </div>
         </div>
         <div className="flex items-center gap-x-4">
-          <h3 className="font-semibold text-black">Color:</h3>
-          <div className="h-6 w-6 rounded-full border border-gray-600" 
+          <h3 className="font-semibold text-black dark:text-white">Color:</h3>
+          <div className="h-6 w-6 rounded-full border border-gray-600 dark:border-white" 
           style={{ backgroundColor: data?.color?.value }} />
         </div>
       </div>
