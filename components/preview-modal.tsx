57b55d03b75a66fa6bf2d196d5d8eabb9d752e46
Jack Slice <jackslice9@gmail.com>
2023-08-05 22:39:56 -0300

    Dark mode preview model fixed

--- components/preview-modal.tsx
+++ components/preview-modal.tsx
@@ -18,7 +18,7 @@ const PreviewModal = () => {
         open={previewModal.isOpen}
         onClose={previewModal.onClose}
         >
-            <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
+            <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8 dark:bg-slate-800">
                 <div className="sm:col-span-4 lg:col-span-5">
                     <Gallery images={product.images} />
                 </div>
--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -18,7 +18,7 @@ const Modal: React.FC<ModalProps> = ({
 }) => {
     return (
         <Transition show={open} appear as={Fragment}>
-            <Dialog as="div" className="relative z-10" onClose={onClose}>
+            <Dialog as="div" className="relative z-10 " onClose={onClose}>
                 <div className="fixed inset-0 bg-black bg-opacity-50" />
 
                 <div className="fixed inset-0 overflow-y-auto">
@@ -34,7 +34,7 @@ const Modal: React.FC<ModalProps> = ({
                         leaveTo="opacity-0 scale-95"
                         >
                             <Dialog.Panel className="h-full max-w-3xz overflow-hidden rounded-lg text-left align-middle">
-                                <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+                                <div className="relative flex w-full items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8 dark:bg-slate-800">
                                     <div className="absolute right-4 top-4">
                                         <IconButton onClick={onClose} icon={<X size={15} />} />
                                     </div>
